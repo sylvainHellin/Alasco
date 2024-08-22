fd9b530e62c7bfa0fb99617ee8d9c02fdd9c7003
sylvain hellin <hellinsylvain@gmail.com>
2024-08-22 08:54:13 +0200

    test workflow deployment.

--- Alasco/__init__.py
+++ Alasco/__init__.py
@@ -1,11 +1,15 @@
 """
 alasco - A Python module to facilitate interaction with the Alasco API.
 
+This is an alpha version for submodules client, data_fetcher, data_transformer, document_downloader and utils. 
+Submodules document_uploader and data_updater are placeholders for now and will be implemented in the future.
+
+
 Author: sylvain hellin
-Version: 0.0.3
+Version: 0.0.4
 """
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "sylvain hellin"
 
 # Import key classes or functions
