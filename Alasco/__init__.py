fef0e567aada11b20410706bf82125c4e26ddc2e
sylvain hellin <hellinsylvain@gmail.com>
2024-08-29 09:15:34 +0200

    updated data_fetch and document_download

--- Alasco/__init__.py
+++ Alasco/__init__.py
@@ -4,16 +4,16 @@ alasco - A Python module to facilitate interaction with the Alasco API.
 This is an alpha version for submodules client, data_fetcher, data_transformer, document_downloader and utils. 
 Submodules document_uploader and data_updater are placeholders for now and will be implemented in the future.
 
-Changelog zur Version 0.0.5:
-- changed wrong implementation of document_downloader.download_change_orders.
-- updated the verbose mode of the document_downloader sub-module.
-- added method to download all documents from a project: document_downloader.batch_download_documents.
+Changelog zur Version 0.0.7:
+- moved get_documents methods to data_fetcher
+- moved _prepaer_urls methods to utils
+- added upload_document methods 
 
 Author: sylvain hellin
-Version: 0.0.6
+Version: 0.0.7
 """
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __author__ = "sylvain hellin"
 
 # Import key classes or functions
--- Alasco/client.py
+++ Alasco/client.py
@@ -8,10 +8,22 @@ from alasco.document_uploader import DocumentUploader
 from alasco.utils import Utils
 
 class Alasco:
-	def __init__(self, token, key, verbose = False, download_path: str | None = None):
+	def __init__(self, token: str, key: str, verbose = False, download_path: str | None = None):
+		"""
+		Initializes an instance of the Alasco class.
+
+		Parameters:
+			token (str): The token used for authentication.
+			key (str): The key used for authentication.
+			verbose (bool): A flag indicating whether to enable verbose mode. Defaults to False.
+			download_path (str | None): The path where documents will be downloaded. Defaults to None.
+
+		Returns:
+			None
+		"""
 
 		self.verbose = verbose
-		
+
 		self.utils = Utils()
 		self.header = self.utils.headerParameters(token=token, key=key)
 
@@ -20,5 +32,3 @@ class Alasco:
 		self.data_updater = DataUpdater(self.header, verbose=verbose)
 		self.document_downloader = DocumentDownloader(header=self.header, verbose=verbose, download_path=download_path)
 		self.document_uploader = DocumentUploader(header=self.header, verbose=verbose)
-
-
