918405ad681b409fe5d18c655fd69466b2629315
sylvain hellin <hellinsylvain@gmail.com>
2025-05-19 16:53:41 +0200

    created two new methods: create_contract and update_contract

--- Alasco/__init__.py
+++ Alasco/__init__.py
@@ -1,14 +1,14 @@
 """
 alasco - A Python module to facilitate interaction with the Alasco API.
 
-This is an alpha version for submodules client, data_fetcher, data_transformer, document_downloader and utils. 
+This is an alpha version for submodules client, data_fetcher, data_transformer, document_downloader and utils.
 Submodules document_uploader and data_updater are placeholders for now and will be implemented in the future.
 
 Changelog zur Version 0.0.7:
 - moved get_documents methods to data_fetcher
 - moved _prepaer_urls methods to utils
 - add .drop_duplicates() after consolidating data in data_transformer
-- added upload_document methods 
+- added upload_document methods
 
 TODO:
 - N.A.
@@ -22,3 +22,6 @@ __author__ = "sylvain hellin"
 
 # Import key classes or functions
 from .client import Alasco
+from .data_updater import Contract
+
+__all__ = ["Alasco", "Contract"]
