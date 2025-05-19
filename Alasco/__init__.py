28747367ed9010b497c3a55229f7ad8afb897d29
sylvain hellin <hellinsylvain@gmail.com>
2025-05-19 16:56:45 +0200

    new version

--- Alasco/__init__.py
+++ Alasco/__init__.py
@@ -4,20 +4,17 @@ alasco - A Python module to facilitate interaction with the Alasco API.
 This is an alpha version for submodules client, data_fetcher, data_transformer, document_downloader and utils.
 Submodules document_uploader and data_updater are placeholders for now and will be implemented in the future.
 
-Changelog zur Version 0.0.7:
-- moved get_documents methods to data_fetcher
-- moved _prepaer_urls methods to utils
-- add .drop_duplicates() after consolidating data in data_transformer
-- added upload_document methods
+Changelog to Version 0.0.8:
+- added new methods to create and update contracts in data_updater
 
 TODO:
 - N.A.
 
 Author: sylvain hellin
-Version: 0.0.8
+Version: 0.0.9
 """
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 __author__ = "sylvain hellin"
 
 # Import key classes or functions
